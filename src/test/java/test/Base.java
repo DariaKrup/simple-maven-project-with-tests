618450653b72e12244398d461ab51b524185e66d
dkrupkina <dariakrupkina@gmail.com>
2023-10-23 15:47:15 +0200

    Change on fail comment

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -32,9 +32,9 @@ class Base {
     protected void run() {
         double r = Math.random();
         if (r < 0.1) {
-            fail("oops");
+            fail("Test is failed");
         } else if (r < 0.2) {
-            throw new AssumptionViolatedException("skipping test");
+            throw new AssumptionViolatedException("Skipping test");
         }
     }
 
