cafdb28dea83cf6018062d6897a2f95634f73523
dkrupkina <dkrupkina@TeamCity>
2023-10-26 09:28:13 +0000

    TeamCity change in 'Simple Maven Project With Tests' project: general settings of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -11,6 +11,11 @@ To apply the patch, change the buildType with id = 'Build'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Build")) {
+    check(artifactRules == "") {
+        "Unexpected option value: artifactRules = $artifactRules"
+    }
+    artifactRules = "**/* => sources.zip"
+
     expectSteps {
         maven {
             goals = "clean test"
